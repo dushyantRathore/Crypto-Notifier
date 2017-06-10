4f65e82933becca2190b81656952dca49b217381
dushyantRathore <dushyant.bgs@gmail.com>
2017-06-11 01:20:37 +0530

    Initial

---
+++ Main_notify.py
@@ -0,0 +1,26 @@
+import notify2
+import Rates
+
+
+def notify():
+    bitcoin = Rates.fetch_bitcoin()
+    ethereum = Rates.fetch_ethereum()
+    litecoin = Rates.fetch_litecoin()
+    monero = Rates.fetch_monero()
+
+    result = ""
+    result = result + str(bitcoin[0]) + "  " + str(bitcoin[2].encode('utf-8')) + "\n"
+    result = result + str(ethereum[0]) + "  " + str(ethereum[2].encode('utf-8')) + "\n"
+    result = result + str(litecoin[0]) + "  " + str(litecoin[2].encode('utf-8')) + "\n"
+    result = result + str(monero[0]) + "  " + str(monero[2].encode('utf-8')) + "\n"
+
+    # Notification Tool
+
+    notify2.init("Cryptocurrency rates notifier")
+    n = notify2.Notification("Crypto Notifier")
+    n.update("Rates Details", result)
+    n.show()
+
+
+if __name__ == "__main__":
+    notify()
---
+++ Rates.py
@@ -0,0 +1,72 @@
+from bs4 import BeautifulSoup
+import urllib2
+import requests
+
+
+def fetch_bitcoin():
+    url = "https://www.coingecko.com/en/price_charts/bitcoin/inr"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    bitcoin_file = requests.get(url)
+    soup = BeautifulSoup(bitcoin_file.text, "html.parser")
+
+    bitcoin_li = []
+
+    for table in soup.find_all("table", attrs={"class" : "table"}):
+        for td in table.find_all("td"):
+            bitcoin_li.append(td.text)
+
+    del bitcoin_li[3:]
+
+    return bitcoin_li
+
+
+def fetch_ethereum():
+    url = "https://www.coingecko.com/en/price_charts/ethereum/inr"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    ethereum_file = requests.get(url)
+    soup = BeautifulSoup(ethereum_file.text, "html.parser")
+
+    ethereum_li = []
+
+    for table in soup.find_all("table", attrs={"class": "table"}):
+        for td in table.find_all("td"):
+            ethereum_li.append(td.text)
+
+    del ethereum_li[3:]
+
+    return ethereum_li
+
+
+def fetch_litecoin():
+    url = "https://www.coingecko.com/en/price_charts/litecoin/inr"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    litecoin_file = requests.get(url)
+    soup = BeautifulSoup(litecoin_file.text, "html.parser")
+
+    litecoin_li = []
+
+    for table in soup.find_all("table", attrs={"class": "table"}):
+        for td in table.find_all("td"):
+            litecoin_li.append(td.text)
+
+    del litecoin_li[3:]
+
+    return litecoin_li
+
+
+def fetch_monero():
+    url = "https://www.coingecko.com/en/price_charts/monero/inr"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    monero_file = requests.get(url)
+    soup = BeautifulSoup(monero_file.text, "html.parser")
+
+    monero_li = []
+
+    for table in soup.find_all("table", attrs={"class": "table"}):
+        for td in table.find_all("td"):
+            monero_li.append(td.text)
+
+    del monero_li[3:]
+
+    return monero_li
+
